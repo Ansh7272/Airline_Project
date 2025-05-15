ba2b451646f1fa8276dd3cc35d119fc361a03a73
Anshu Gupta <anshu8111121@gmail.com>
2025-05-15 13:07:17 +0530

    commited simple dotenv

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const {PORT} = require('./config/serverconfig')
+const {PORT} = require('./config/serverConfig')
 const setupAndStartServer= async () =>{
 
+    const PORT = process.env.PORT
     const app =express();
     app.listen(PORT,() =>{
         console.log(`server started at ${PORT}`);
-        console.log(process.env);
+        // console.log(process.env.NAME)
+        // console.log(process.env);
     });
 }
 setupAndStartServer();
